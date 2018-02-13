2ffcb7426d4984906f5fbad13b76687e87daa9d8
Radosław Gliński <gliniak93@gmail.com>
2018-02-13 18:36:19 +0100

    [0.24] Some changes, some reverts

--- app/src/main/java/com/lujuf/stado/mixup/MyProfileFragment.java
+++ app/src/main/java/com/lujuf/stado/mixup/MyProfileFragment.java
@@ -13,8 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-//import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,8 +23,9 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+
+//import android.widget.ExpandableListAdapter;
 
 /**
  * Created by Gliniak on 09.01.2018.
@@ -39,16 +38,10 @@ public class MyProfileFragment extends Fragment {
     private Button dummy_song_button;
     private Button show_songs;
 
-
     private RecyclerView songs_view;
 
     private List<FirebaseDatabaseObject.DatabaseSongs> songsList = new ArrayList<>();
     private DatabaseSongsAdapter mAdapter;
-    private ExpandableListView expListView;
-
-    ExpandableListAdapter listAdapter;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
 
     @Override
     public void onAttach(Context context) {
@@ -63,49 +56,8 @@ public class MyProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-        expListView = (ExpandableListView)rootView.findViewById(R.id.lvExp);
         return rootView;
     }
-
-    private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
-        // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
-
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
-
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
-    }
-
 
     //@Override
     //public void onAttach(Context context) {
@@ -116,15 +68,6 @@ public class MyProfileFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
-        // preparing list data
-        prepareListData();
-
-        // FIX THIS!!!
-        listAdapter = new ExpandableListAdapter(getActivity(), listDataHeader, listDataChild);
-
-        // setting list adapter
-       expListView.setAdapter(listAdapter);
 
         mDatabase = FirebaseDatabase.getInstance();
 
