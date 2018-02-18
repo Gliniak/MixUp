c2abb58a4fb213b1ed6f60a4b1bbe61af1b0a981
mainstreamowy <mainstreamowy@gmail.com>
2018-02-18 23:25:27 +0100

    Upgrade'd music player

--- app/src/main/java/com/lujuf/stado/mixup/Fragments/AddSongsFragment.java
+++ app/src/main/java/com/lujuf/stado/mixup/Fragments/AddSongsFragment.java
@@ -78,9 +78,11 @@ public class AddSongsFragment extends Fragment {
     StorageReference storageRef = storage.getReference();
 
     public static final int EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 2;
+    public static final int MY_PERMISSIONS_REQUEST_READ = 10;
 
     private ProgressDialog mProgressDialog;
     private File mFile;
+
     Uri selectedUri;
     Uri songUrl;
 
@@ -297,6 +299,32 @@ public class AddSongsFragment extends Fragment {
                 boolean fileNotNull = mFile != null;
                 boolean fileExist = mFile.exists();
                 boolean canRead = mFile.canRead();
+
+                if (ContextCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.READ_CONTACTS)
+                        != PackageManager.PERMISSION_GRANTED) {
+
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                            Manifest.permission.READ_CONTACTS)) {
+
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+
+                    } else {
+
+                        // No explanation needed, we can request the permission.
+
+                        ActivityCompat.requestPermissions(getActivity(),
+                                new String[]{Manifest.permission.READ_CONTACTS},
+                                MY_PERMISSIONS_REQUEST_READ);
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int constant. The callback method gets the
+                        // result of the request.
+                    }
+                }
 
                 if (fileNotNull && fileExist) {
                     InputStream stream = null;
--- app/src/main/java/com/lujuf/stado/mixup/Fragments/PlayerFragment.java
+++ app/src/main/java/com/lujuf/stado/mixup/Fragments/PlayerFragment.java
@@ -2,16 +2,24 @@ package com.lujuf.stado.mixup.Fragments;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.lujuf.stado.mixup.R;
+
+import static com.lujuf.stado.mixup.MainScreenActivity.context;
 
 /**
  * Created by Gliniak on 18.02.2018.
@@ -23,7 +31,12 @@ public class PlayerFragment extends Fragment {
     ImageButton nextSong;
     ImageButton playSong;
     ImageButton pauseSong;
-
+    TextView elapsedTimeLabel;
+    TextView remainingTimeLabel;
+    SeekBar positionBar;
+    MediaPlayer mp;
+    int totalTime;
+    Uri requestedSongUri;
     @Override
     public void onAttach(Context context) {
         Log.d("GUI", "Avatar onAttach!");
@@ -45,6 +58,34 @@ public class PlayerFragment extends Fragment {
     //    super.onAttach(context);
     // }
 
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg){
+            int currentPosition=msg.what;
+            //Update positionBar
+            positionBar.setProgress(currentPosition);
+
+            //Update labels
+            String elapsedTime = createTimeLabel(currentPosition);
+            elapsedTimeLabel.setText(elapsedTime);
+
+            String remainingTime = createTimeLabel(totalTime-currentPosition);
+            remainingTimeLabel.setText("- " + remainingTime);
+        }
+    };
+
+    public String createTimeLabel(int time){
+        String timeLabel ="";
+        int min = time / 1000/60;
+        int sec = time/1000%60;
+
+        timeLabel = min + ":";
+        if (sec<10) timeLabel += "0";
+        timeLabel+=sec;
+
+        return timeLabel;
+    }
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
@@ -57,6 +98,59 @@ public class PlayerFragment extends Fragment {
         nextSong = view.findViewById(R.id.player_next_song);
         playSong = view.findViewById(R.id.player_play_button);
         pauseSong = view.findViewById(R.id.player_stop_button);
+
+        elapsedTimeLabel=(TextView)getView().findViewById(R.id.timePlayed);
+        remainingTimeLabel=(TextView)getView().findViewById(R.id.timeRemaining);
+
+        // Mediaplayer
+        // Kiedy bedzie skonczony upload plików wypełnic requestedSongUri
+
+        mp =MediaPlayer.create(context,requestedSongUri);
+        mp.setVolume(0.5f,0.5f);
+        totalTime = mp.getDuration();
+
+        //PositionBar
+
+        positionBar=(SeekBar)getView().findViewById(R.id.positionBar);
+        positionBar.setMax(totalTime);
+        positionBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if(fromUser){
+                            mp.seekTo(progress);
+                            positionBar.setProgress(progress);
+                        }
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        // Thread (Update positionBar & timeLabel)
+        new Thread(new Runnable(){
+                @Override
+                        public void run(){
+                    while(mp!=null){
+                        try{
+                            Message msg =new Message();
+                            msg.what =mp.getCurrentPosition();
+                            handler.sendMessage(msg);
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {}
+                    }
+                }
+        }).start();
+
+
 
         prevSong.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,8 +169,17 @@ public class PlayerFragment extends Fragment {
         playSong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                playSong.setVisibility(View.INVISIBLE);
-                pauseSong.setVisibility(View.VISIBLE);
+                if(!mp.isPlaying())
+                {
+                    mp.start();
+                    playSong.setVisibility(View.INVISIBLE);
+                    pauseSong.setVisibility(View.VISIBLE);
+                } else {
+                    mp.pause();
+                    pauseSong.setVisibility(View.INVISIBLE);
+                    playSong.setVisibility(View.VISIBLE);
+                }
+
             }
         });
 
